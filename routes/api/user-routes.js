6340b1333c99c772da19fa508f95f3878cb31da9
Janelle <janellephalon@gmail.com>
2022-04-24 02:05:12 -0400

    updated routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controller/user-controller');
+
+router 
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router 
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route('/:userId/friend/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;

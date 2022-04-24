8f8fc53cbb0cc49672418fb5b6d5305e88fd2157
Janelle <janellephalon@gmail.com>
2022-04-24 01:23:21 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        require: true, 
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        match: ['/.+\@.+\..+/'],
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+});
+
+// Total Count of Friends
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
